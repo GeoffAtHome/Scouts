2e86b6005fb43760413b63d081b37b8a91827928
GeoffAtHome <geoff@soord.org.uk>
2021-08-20 13:30:15 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    if (action == 1) {
+        game.addScore(1)
+        action = 0
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (action == 2) {
+        game.addScore(1)
+        action = 0
+    }
+})
+let action = 0
+game.startCountdown(10000)
+game.setScore(0)
 basic.forever(function () {
-	
+    action = randint(1, 2)
+    if (action == 1) {
+        basic.showString("A")
+    } else {
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . # . . .
+            # . . . .
+            `)
+        while (action > 0) {
+            basic.pause(100)
+        }
+    }
 })
